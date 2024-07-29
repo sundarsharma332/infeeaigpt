1f943d4ebca62cf8fdc4c8ed8d731b36ff00ae27
sundar Raj Sharma <sundarsharma469@gmail.com>
2024-07-29 15:31:17 +0530

    style update

---
+++ .history/js/app_20240729152918.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Smooth scroll for navigation links
+    document.querySelectorAll('a.nav-link').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
